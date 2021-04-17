0b51e5b1be8030b6b75f50b60a8a7c5c87c3fe98
Sourabh Kushwaha <sourabhkk95@gmail.com>
2021-04-17 12:37:29 +0530

    using components in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import Block from './Block/Block'
+import Adder from './Adder/Adder'
+import { useEffect, useState } from 'react';
+import SidebarActions from './SidebarActions/SidebarActions';
+import Conditional from './Conditional/Conditional';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	function retrieveFromStorage() {
+		return JSON.parse(localStorage.getItem("data"));
+	}
+
+	const [data, changeData] = useState([]);
+	const [firstRender, changefirstRender] = useState(true);
+
+	useEffect(()=>{
+		if(firstRender){
+			changeData(retrieveFromStorage() || []);
+			changefirstRender(false);
+		}
+		else{
+			saveToStorage();
+		}
+	})
+
+	function changeDataHandler(conditionPos, index, type) {
+		changeData(data => updateData(data, conditionPos, index, type));
+	}
+
+	function saveToStorage() {
+		localStorage.setItem("data", JSON.stringify(data));
+	}
+
+	function updateData(data, conditionPos, index, type) {
+		if (conditionPos.length) {
+			const [conditionType, conditionIndex] = conditionPos[0];
+			return [
+				...data.slice(0, conditionIndex),
+				{
+					...data[conditionIndex],
+					[conditionType]: updateData(data[conditionIndex][conditionType], conditionPos.slice(1), index, type)
+				},
+				...data.slice(conditionIndex + 1)
+			];
+		}
+		else {
+			if (type == 4) {
+				return [...data.slice(0, index), { if: data.slice(index), else: [] }];
+			}
+			else return [...data.slice(0, index), Number(type), ...data.slice(index)];
+		}
+	}
+
+	function renderComp(data, conditionPos = []) {
+		return (<>
+			<Adder index={0} changeDataHandler={changeDataHandler} conditionPos={conditionPos} />
+			{
+				data.map((ele, i) => {
+					if (typeof ele === "number") {
+						return <>
+
+							<Block type={ele} />
+							<Adder index={i + 1} changeDataHandler={changeDataHandler} conditionPos={conditionPos} />
+
+						</>
+					}
+					else {
+						return (
+							<Conditional element={ele} conditionPos={conditionPos} index={i} renderComp={renderComp} />
+						)
+					}
+				})
+			}
+		</>
+		)
+	}
+	
+	return (
+		<div className="container">
+			<section>
+					{renderComp(data)}
+			</section>
+			<SidebarActions />
+		</div>
+	)
 }
 
 export default App;
